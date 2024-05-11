fae32bd84f9f892a8817cdbac6960250ad0ce399
Maaz <83403349+Maaz-319@users.noreply.github.com>
2024-05-11 19:59:17 +0500

    In Progress

---
+++ Library Management System/database_handler.py
@@ -0,0 +1,90 @@
+import sqlite3
+
+
+def create_table():
+    connection = sqlite3.connect("data.db")
+    cur = connection.cursor()
+
+    cur.execute("""CREATE TABLE Book(
+		book_name text,
+		book_author text,
+		book_genre text
+	)""")
+    print("Done")
+
+    connection.commit()
+    connection.close()
+
+
+def add_record(name, author, genre):
+    connection = sqlite3.connect("data.db")
+    cur = connection.cursor()
+
+    try:
+        cur.execute("INSERT INTO Book VALUES(?,?,?)", (name, author, genre))
+    except:
+        create_table()
+        add_record(name, author, genre)
+
+    connection.commit()
+    connection.close()
+
+
+def show_all():
+    connection = sqlite3.connect("data.db")
+    cur = connection.cursor()
+
+    try:
+        return cur.execute("SELECT * FROM Book ORDER BY book_name ASC").fetchall()
+    except:
+        create_table()
+        show_all()
+
+    connection.commit()
+    connection.close()
+
+
+def show_one(word):
+    connection = sqlite3.connect("data.db")
+    cur = connection.cursor()
+
+    try:
+        cur.execute("SELECT * FROM Book WHERE book_name=?", (word,))
+        results = cur.fetchall()
+
+        if results:
+            return results
+        else:
+            return False
+    except sqlite3.Error as e:
+        return e
+        create_table()
+
+    connection.commit()
+    connection.close()
+
+
+def update_record(name, author, genre):
+    connection = sqlite3.connect("data.db")
+    cur = connection.cursor()
+
+    try:
+        cur.execute("UPDATE Book SET book_author = ? WHERE book_name = ?", (author, name))
+    except:
+        create_table()
+
+    connection.commit()
+    connection.close()
+
+
+def delete_record(name):
+    connection = sqlite3.connect("data.db")
+    cur = connection.cursor()
+
+    try:
+        cur.execute("DELETE from Book WHERE book_name=?", (name,))
+    except:
+        create_table()
+
+    connection.commit()
+    connection.close()
